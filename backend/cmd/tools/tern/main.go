c52ace771b534be7e0b639079da071c5e0ebb404
João Pedro <108898728+JP-Go@users.noreply.github.com>
2024-08-07 17:21:28 +0000

    Finalize script to execute migrations

---
+++ backend/cmd/tools/tern/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load("../.env"); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+	buf := bytes.Buffer{}
+	cmd.Stderr = &buf
+	if err := cmd.Run(); err != nil {
+		fmt.Println(buf.String())
+		panic(err)
+	}
+}
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/JP-Go/semana-go-react/backend
 
 go 1.21.12
+
+require github.com/joho/godotenv v1.5.1
